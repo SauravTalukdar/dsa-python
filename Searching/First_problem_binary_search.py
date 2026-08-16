0870a7946f2282a32daa49efb0c344e50aa0cff6
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-16 14:29:39 +0530

    Listed the possible test/edge cases

--- Searching/First_problem_binary_search.py
+++ Searching/First_problem_binary_search.py
@@ -15,7 +15,37 @@ def locate_card(cards,find):
             low = mid + 1 
     return -1    
 
+# test cases
 
+# 1.Normal card found at a position
+cards = [13,12,11,9,7,5,4,2,0]
+find = 5
+print(locate_card(cards,find))
+
+# 2.Empty deck
+cards = []
+find = 5
+print(locate_card(cards,find))
+
+# 3.Repeating numbers
+cards = [13,12,11,11,9,9,7,5,4,2,0]
+find = 5
+print(locate_card(cards,find))
+
+# 4.Number to find is repeating
+cards = [13,12,11,9,7,5,5,4,2,0]
+find = 5
+print(locate_card(cards,find))
+
+# 5.Number at last position
+cards = [13,12,11,9,7,5]
+find = 5
+print(locate_card(cards,find))
+
+# 6.Number found at first position
+cards = [13,12,11,9,7,5,4,2,0]
+find = 13
+print(locate_card(cards,find))
 
 
 
