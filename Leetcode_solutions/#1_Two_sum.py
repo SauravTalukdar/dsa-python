a4343817e5f38885a124949da4c3729150dffa84
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-19 00:32:45 +0530

    Optimized the brute force soluction

--- Leetcode_solutions/#1_Two_sum.py
+++ Leetcode_solutions/#1_Two_sum.py
@@ -1,15 +1,10 @@
 # the brute for way to do it(linearly)
 def two_sum(nums,target):
-    fir = 0
-    sec = 1
-    while(fir<=len(nums)-1):
-        if nums[fir] + nums[sec] == target:
-            return fir,sec
-        else:
-            fir += 1
-            sec += 1
-    return -1
-
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] + nums[j] == target:
+                return i,j
+    return -1            
 nums = [2,7,11,15]
 target = 9
 print(two_sum(nums,target))
@@ -21,4 +16,9 @@ print(two_sum(nums,target))
 nums = [3,3]
 target = 6
 print(two_sum(nums,target))
+
+nums = [2,7,11,15]
+target = 13
+print(two_sum(nums,target))
+
 
