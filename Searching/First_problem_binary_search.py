bc1f5f3e64d2cd2a9a697f5f2b696a96b347191f
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-19 19:29:58 +0530

    Formated the codes

--- Searching/First_problem_binary_search.py
+++ Searching/First_problem_binary_search.py
@@ -1,53 +1,53 @@
-# # doing the card problem with binary search
+# doing the card problem with binary search
 
-# def locate_card(cards,find):
+def locate_card(cards,find):
     
-#     low = 0
-#     high = len(cards) - 1
-#     while(low<=high):
-#         mid = (low+high)// 2
-#         mid_number = cards[mid]
-#         if mid_number == find:
-#             return mid
-#         elif mid_number > find:
-#             low = mid + 1   
-#         elif mid_number < find:
-#             high = mid - 1
-#     return -1 
+    low = 0
+    high = len(cards) - 1
+    while(low<=high):
+        mid = (low+high)// 2
+        mid_number = cards[mid]
+        if mid_number == find:
+            return mid
+        elif mid_number > find:
+            low = mid + 1   
+        elif mid_number < find:
+            high = mid - 1
+    return -1 
             
 
 
-# # test cases
+# test cases
 
-# # 1.Normal card found at a position
-# cards = [13,12,11,9,7,5,4,2,0]
-# find = 5
-# print(locate_card(cards,find))
+# 1.Normal card found at a position
+cards = [13,12,11,9,7,5,4,2,0]
+find = 5
+print(locate_card(cards,find))
 
-# # 2.Empty deck
-# cards = []
-# find = 5
-# print(locate_card(cards,find))
+# 2.Empty deck
+cards = []
+find = 5
+print(locate_card(cards,find))
 
-# # 3.Repeating numbers
-# cards = [13,12,11,11,9,9,7,5,4,2,0]
-# find = 5
-# print(locate_card(cards,find))
+# 3.Repeating numbers
+cards = [13,12,11,11,9,9,7,5,4,2,0]
+find = 5
+print(locate_card(cards,find))
 
-# # 4.Number to find is repeating
-# cards = [13,12,11,9,7,5,5,4,2,0]
-# find = 5
-# print(locate_card(cards,find))
+# 4.Number to find is repeating
+cards = [13,12,11,9,7,5,5,4,2,0]
+find = 5
+print(locate_card(cards,find))
 
-# # 5.Number at last position
-# cards = [13,12,11,9,7,5]
-# find = 5
-# print(locate_card(cards,find))
+# 5.Number at last position
+cards = [13,12,11,9,7,5]
+find = 5
+print(locate_card(cards,find))
 
-# # 6.Number found at first position
-# cards = [13,12,11,9,7,5,4,2,0]
-# find = 13
-# print(locate_card(cards,find))
+# 6.Number found at first position
+cards = [13,12,11,9,7,5,4,2,0]
+find = 13
+print(locate_card(cards,find))
 
 
 # 7.handling an edge case where there multiple numbers repeating and our find number is also repeating(finding its first occurence)
@@ -82,7 +82,7 @@ def locate_card(cards,find):
         elif result == "left":
             high = mid - 1
     return -1
-    
+
 # example  
 cards = [8,8,6,6,6,6,6,6,3,2,2,2,0,0,0]
 find = 6
--- Searching/Linear_search.py
+++ Searching/Linear_search.py
@@ -1,26 +1,26 @@
-# # using a while loop
+# using a while loop
 
-# linear = [1,4,5,7]
-# find = 2
-# i = 0
-# while i<len(linear):
-#     if linear[i] == find:
-#         print(f"The element {find} is found at index {i}")
-#         break
-#     i+=1  
-# else:
-#     print("Element not found")
+linear = [1,4,5,7]
+find = 2
+i = 0
+while i<len(linear):
+    if linear[i] == find:
+        print(f"The element {find} is found at index {i}")
+        break
+    i+=1  
+else:
+    print("Element not found")
 
-# # using a for loop
+# using a for loop
 
-# elements = [1,5,22,7,55,3]
-# searching = 5
-# for i in range(0,len(elements)):
-#     if elements[i] == searching:
-#         print(f"Element {searching} found at {i+1} position")
-#         break
-# else:
-#     print("Element not found")
+elements = [1,5,22,7,55,3]
+searching = 5
+for i in range(0,len(elements)):
+    if elements[i] == searching:
+        print(f"Element {searching} found at {i+1} position")
+        break
+else:
+    print("Element not found")
 
 
 # finding a card of a specific number in a deck of cards(linear search method)
