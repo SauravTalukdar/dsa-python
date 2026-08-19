1fa1157fdeee09d44b3663804d39156cfd53e571
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-19 19:00:26 +0530

    Wrote the brute force solution with test cases

--- Leetcode_solutions/#1_Two_sum.py
+++ Leetcode_solutions/#1_Two_sum.py
@@ -1,24 +1,42 @@
-# the brute for way to do it(linearly)
+# the brute for way to do it
+
 def two_sum(nums,target):
-    for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
+    last = len(nums)
+    for i in range((last)):
+        for j in range(i+1,last):
             if nums[i] + nums[j] == target:
-                return i,j
-    return -1            
+                return [i,j]
+    return -1 
+
+# test cases
+
+# 1.
 nums = [2,7,11,15]
 target = 9
 print(two_sum(nums,target))
 
+#2.
 nums = [3,2,4]
 target = 6
 print(two_sum(nums,target))
 
+#3.
 nums = [3,3]
 target = 6
 print(two_sum(nums,target))
 
+#4.
 nums = [2,7,11,15]
 target = 13
 print(two_sum(nums,target))
+
+#5.
+nums = []
+target = 6
+print(two_sum(nums,target))
+
+
+
+           
 
 
