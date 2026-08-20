2416da9047628781fc91d8ae6e3d52ff24d074df
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-20 21:51:42 +0530

    Wrote binary search way to find last negative and first positive location

--- Leetcode_solutions/#2529_maxcount_pos_or_neg.py
+++ Leetcode_solutions/#2529_maxcount_pos_or_neg.py
@@ -3,8 +3,8 @@
 # Difficulty: Easy
 # Time complexity: O(n)
 # Space complexity: O(1)
-# Approch: Brute force
 
+# Approch: Brute force
 def max_count(nums):
     pos = 0
     neg = 0
@@ -19,6 +19,51 @@ def max_count(nums):
 print(max_count([-2,-1,-1,1,2,3]))
 print(max_count([-3,-2,-1,0,0,1,2]))
 print(max_count([5,20,66,1314]))
+
+# Approch: Binary search for finding last negative and finding first positive
+def maximum(nums):
+    def neg(nums):
+        low = 0
+        high = len(nums) - 1
+        result = -1
+        while(low<=high):
+            mid = (low+high) // 2
+            mid_number = nums[mid]
+            if mid_number < 0:
+                result = mid
+                low = mid + 1
+            else:
+                high = mid - 1            
+        return result 
+
+    def pos(nums):
+        low = 0
+        high = len(nums) - 1
+        result = len(nums)
+        while(low<=high):
+            mid = (low+high) // 2
+            mid_number = nums[mid]
+            if mid_number > 0:
+                result = mid
+                high = mid - 1
+            else:
+                low = mid + 1    
+        return result     
+
+    negative = neg(nums) + 1
+    positive = len(nums) - pos(nums) 
+    return max(negative,positive)
+
+
+print(maximum([-2,-1,-1,1,2,3]))
+print(maximum([-3,-2,-1,0,0,1,2]))
+print(maximum([5,20,66,1314]))
+print(maximum([0,0,0]))
+   
+
+
+            
+
 
 
 
