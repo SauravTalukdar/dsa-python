2aa6037ce69218799327902725fd781c35b60219
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-22 15:59:19 +0530

    leetcode #153 - find minimum in rotated array, second(better) approach

--- Leetcode_solutions/#153_Min_in_rotatedSortedArray.py
+++ Leetcode_solutions/#153_Min_in_rotatedSortedArray.py
@@ -21,19 +21,34 @@ def findmin_v1(nums):
             high = mid - 1
     return nums[low] #this method wont handle case 6 (only one element)       
 
+# Approach 2 - mid vs high comparison  
+# cleaner, handles all cases through same logic, no patches needed
 
+def findmin_v2(nums):
+    low = 0
+    high = len(nums) - 1
+    while(low<=high):
+        mid = (low+high) // 2
+        mid_number = nums[mid]
+        if low == high:
+            return mid_number
+        elif mid_number > nums[high]:
+            low = mid + 1
+        else:
+            high = mid 
+    return -1        
 
 # test_cases
 #1.
-print(findmin_v1([11,12,13,15,17]))                               
+print(findmin_v2([11,12,13,15,17]))                               
 #2.
-print(findmin_v1([3,4,5,1,2]))                                                     
+print(findmin_v2([3,4,5,1,2]))                                                     
 #3.
-print(findmin_v1([4,5,6,7,0,1,2]))                                                            
+print(findmin_v2([4,5,6,7,0,1,2]))                                                            
 #4.
-print(findmin_v1([12,13,15,17,10]))  
+print(findmin_v2([12,13,15,17,10]))  
 #5.
-print(findmin_v1([]))
+print(findmin_v2([]))
 #6.  
-print(findmin_v1([1]))  
+print(findmin_v2([1]))  
                              
