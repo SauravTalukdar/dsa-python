8771f3833ef9af25f326b89219fa762d7e1d4210
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-22 13:35:06 +0530

    rotated list - Added a another version of Binary search to solve the same problem

--- Searching/Rotating_a_list_BS.py
+++ Searching/Rotating_a_list_BS.py
@@ -4,7 +4,8 @@
 # Space Complexity: O(1)
 # Approach: Binary search - find index of minimum element compare mid with high to determine which half to search
 
-def rotate(rotated_list):
+# version1
+def rotate1(rotated_list):
     low = 0
     high = len(rotated_list) - 1
     while(low<=high):
@@ -15,21 +16,35 @@ def rotate(rotated_list):
             low = mid + 1
         else:
             high = mid 
-    return - 1
+    return 0
+
+# version2
+def rotate2(rotated_list):
+    low = 0
+    high = len(rotated_list) - 1
+    while(low<=high):
+        mid = (low+high) // 2
+        if mid > 0 and rotated_list[mid] < rotated_list[mid - 1]:
+            return mid
+        elif rotated_list[mid] > rotated_list[high]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return 0
 
 # edge cases
 # 1.
-print(rotate([5,6,9,0,2,3,4]))
+print(rotate1([5,6,9,0,2,3,4]))
 #2.
-print(rotate([4,5,6,9,0,2,3]))
+print(rotate1([4,5,6,9,0,2,3]))
 #3.
-print(rotate([]))
+print(rotate1([]))
 #4.
-print(rotate([7,9,1,3,5]))
+print(rotate1([7,9,1,3,5]))
 #5.
-print(rotate([3,5,7,9,1]))
+print(rotate1([3,5,7,9,1]))
 #6.
-print(rotate([1]))
+print(rotate1([1]))
 
 # Approach: Brute force(linear search) - find index of minimum element compare mid with high to determine which half to search
 def rotated_list(nums):
