18772744030bd7d3692157c16b6c05b40cb4ed09
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-23 13:28:18 +0530

    Solved the problem with binary search

--- Leetcode_solutions/#33_Search_Rotatedsortedarray.py
+++ Leetcode_solutions/#33_Search_Rotatedsortedarray.py
@@ -5,13 +5,36 @@
 # Space complexity: O(1)
 
 # Approch: Brute force solution
-def search(nums,target):
+def search_v1(nums,target):
     position = 0
     while(position<len(nums)):
         if nums[position] == target:
             return position
         position +=1
-    return -1  
+    return -1 
+
+# Approch: Binary search solution
+
+def search(nums,target):
+    low = 0
+    high = len(nums) - 1
+    while(low<=high):
+        mid = (low+high) // 2
+        mid_number = nums[mid]
+        if mid_number == target:
+            return mid
+        elif mid_number >= nums[low]:
+            if nums[low] <= target < nums[mid]:
+                high = mid - 1 
+            else:
+                low = mid + 1
+        elif mid_number < nums[low]:
+            if nums[mid] < target <= nums[high]:
+                low = mid + 1 
+            else:
+                high = mid - 1              
+    return - 1            
+
 
 # test cases    
 # 1.
