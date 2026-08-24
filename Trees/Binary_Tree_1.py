f58051b9144a5e148d97e407453f90553cbceed6
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-08-24 19:47:12 +0530

    Created users with the user class for the input

--- Trees/Binary_Tree_1.py
+++ Trees/Binary_Tree_1.py
@@ -21,5 +21,11 @@ class UserDatabase:
     def list_all(self):
         pass
 
-
+aakash = User('aakash', 'Aakash Rai', 'aakash@example.com')
+biraj = User('biraj', 'Biraj Das', 'biraj@example.com')
+hemanth = User('hemanth', 'Hemanth Jain', 'hemanth@example.com')
+jadhesh = User('jadhesh', 'Jadhesh Verma', 'jadhesh@example.com')
+siddhant = User('siddhant', 'Siddhant Sinha', 'siddhant@example.com')
+sonaksh = User('sonaksh', 'Sonaksh Kumar', 'sonaksh@example.com')
+vishal = User('vishal', 'Vishal Goel', 'vishal@example.com')
 
