38874b3c290834ec0271cbe7d40c2ffbfc8127c7
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-09-06 21:25:40 +0530

    Added description for implementation

--- Trees/Balanced_BST.py
+++ Trees/Balanced_BST.py
@@ -23,15 +23,15 @@ class User: #created a user class to store of users (input)
 #function to create the balanced BST   
 def balanced_bst(data,low = 0,high = None,parent = None):
     if high is None:
-        high = len(data) - 1
+        high = len(data) - 1 #set high to the length of the list
     if low > high:
-        return None
+        return None #if we traverse through all elements return None(no elements left)
     mid = (low + high) // 2
-    key,value = data[mid]
-    root = BSTNode(key,value)
-    root.parent = parent
-    root.left = balanced_bst(data,low,mid - 1,root)        
-    root.right = balanced_bst(data,mid + 1,high,root)
+    key,value = data[mid] #store mid of the list
+    root = BSTNode(key,value) #create root node with mid
+    root.parent = parent #set the parent
+    root.left = balanced_bst(data,low,mid - 1,root)  #also pass the root as the parent for the left tree     
+    root.right = balanced_bst(data,mid + 1,high,root) #also pass the root as the parent for the right tree
     return root    
 
 #creating a list of users with key and value pairs in sorted order
