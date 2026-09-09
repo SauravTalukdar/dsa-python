b85f55e9fea7f987711d26fa6dbf38de8cb6ccc5
SauravTalukdar <sauravtalukdar107@gmail.com>
2026-09-09 19:39:36 +0530

    Added comments for understanding

--- Leetcode_solutions/#450_Delete_node_in_BST.py
+++ Leetcode_solutions/#450_Delete_node_in_BST.py
@@ -16,23 +16,26 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if root is None:
             return None
-        if key < root.val:
-            root.left = self.deleteNode(root.left,key)   
+        elif key < root.val: 
+            root.left = self.deleteNode(root.left,key) #recursively run delete on left subtree
         elif key > root.val:
-            root.right = self.deleteNode(root.right,key)
+            root.right = self.deleteNode(root.right,key) #recursively run delete on right subtree
         else:
-            if root.left is None:
-                return root.right 
-            elif root.right is None:
-                return root.left
-            successor = self.find_min(root.right)
-            root.val = successor.val
-            root.right = self.deleteNode(root.right, successor.val)  
-        return root 
+            if root.left is None: #if root doesnt have left subtree
+                return root.right #make the right its replacement
+            elif root.right is None: #if right is not there
+                return root.left #make the left its replacement
+           
+            succesor = self.find_min(root.right) #find the inorder succesor(smallest element in right subtree)
+            root.val = succesor.val #replace targets value with the sucessors value
+            root.right = self.deleteNode(root.right,succesor.val) #delete the original succesor,after replacement
+        return root #return the root for output and recursion call
 
-    
-    def find_min(self, node):
-        while node.left is not None:
-            node = node.left
-        return node                   
+    def find_min(self,node): #minimum element is always the leftmost element
+        while node.left is not None: #keep going left until u cant anymore
+            node = node.left #constantly assigning node's left to make it the node
+        return node #return the end node(min) 
+
+#the inorder successor is used as a replacement as it is the smallest value in the right subtree
+#so when it replaces the target root it is greater than everything in left subtree and smaller than the right subtree                
                 
